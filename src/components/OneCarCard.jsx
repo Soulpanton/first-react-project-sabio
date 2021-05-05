34d67a344099737ca336120f51d7f220794a476d
Juan Pablo Germosen <69020845+Soulpanton@users.noreply.github.com>
2021-05-04 21:03:56 -0400

    =Juan Pablo Germosen Assessment 2

---
+++ src/components/OneCarCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const OneCardCard = (props) => {
+    return (
+        <div className="card col-md-3 m-1">
+            <div className="card-body">
+                {props.key}
+                <h5 className="card-title">{props.make}</h5>
+                <h5 className="card-text">{props.model}</h5>
+                <h5 className="card-text">{props.year}</h5>
+
+            </div>
+        </div>
+    )
+}
+
+
+export default React.memo(OneCardCard);
